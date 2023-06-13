8acf203c120964dd4c61f97ad7f4e2685866cbea
William Denniss <will@icog.net>
2023-06-12 21:24:45 -0700

    Sync changes from the manuscript

--- Chapter10/10.4_TaskLiveness/health_check.sh
+++ Chapter10/10.4_TaskLiveness/health_check.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Liveness probe for batch process
-# The process writes a logfile every time it runs with the current Unix timestamp.
+# The process writes a logfile every time it runs with the current Unix
+# timestamp.
 
 # Usage: process_liveness.sh <path_to_file>
-# The file must contain only the latest date as a Unix timestamp and no newlines
+# The file must contain only the latest date as a Unix timestamp and no
+# newlines
 
 if ! rundate=$(<$1); then
   echo >&2 failed
