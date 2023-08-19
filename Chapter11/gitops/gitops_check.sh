c1614dcc75364514cfcc0bac8c730c39f038f14b
William Denniss <will@icog.net>
2023-08-19 14:29:38 -0700

    Update linebreaks to match manuscript

--- Chapter11/gitops/gitops_check.sh
+++ Chapter11/gitops/gitops_check.sh
@@ -4,7 +4,8 @@ CURRENT_DIR=`echo "${PWD##*/}"`
 CURRENT_NAMESPACE=`kubectl config view --minify -o=jsonpath='{.contexts[0].context.namespace}'`
 
 if [ "$CURRENT_DIR" != "$CURRENT_NAMESPACE" ]; then
-    >&2 echo "Wrong namespace (currently $CURRENT_NAMESPACE but $CURRENT_DIR expected)"
+    >&2 echo "Wrong namespace (currently $CURRENT_NAMESPACE but " \
+             "$CURRENT_DIR expected)"
     exit 1
 fi
 
